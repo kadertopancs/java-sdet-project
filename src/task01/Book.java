2d332b96052b45e1204315371cafb7957ffa5b58
Gülşah Ayaz <gulsahayaz95@gmail.com>
2026-04-18 00:27:11 +0300

    task1 completed

---
+++ src/task01/Book.java
@@ -0,0 +1,24 @@
+package task01;
+
+public class Book {
+    private String name;
+    private String author;
+
+    public Book(String name, String author) {
+        this.name = name;
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public String toString() {
+        return "Kitap: " + name + " | Yazar: " + author;
+    }
+}
